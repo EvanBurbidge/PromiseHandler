8f3ab9778f59f7116c4c5fba20600ae5b592152d
EvanBurbidge <burbidge@tcd.ie>
2022-04-25 09:09:01 +0100

    add call back for those worrying about performance

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
 
  * @return { Promise<Any> }
  */
-export function awaitWrap(promise) {
+export function awaitWrap(promise, { resolveCallback = () => {}, rejectCallback = () => {} }) {
   return promise.then(function (data) {
+    resolveCallback(data);
     return { error: null, data }
   }).catch(function (error) {
+    rejectCallback(error);
     return { error, data: null }
   })
 }
--- test/awaitWrap.test.js
+++ test/awaitWrap.test.js
@@ -1,5 +1,7 @@
 import { awaitWrap } from '../index';
 
+const fn = jest.fn();
+
 describe('awaitWrap', () => {
 
   it('should return a resolved value', async () => {
@@ -20,6 +22,24 @@ describe('awaitWrap', () => {
 
     expect(data).toBe(null)
     expect(error).toBe(testReject);
+  });
+  
+  it('should call the resolve callback', async () => {
+    const testResolveCallback = "Hi there";
+    const promise = Promise.resolve(testResolveCallback);
+
+    const { data, error } = await awaitWrap(promise, { resolveCallback: fn });
+
+    expect(fn).toHaveBeenCalledWith(testResolveCallback);
+  });
+  it('should call the reject callback', async () => {
+    const testResolveCallback = "Hi there";
+    const promise = Promise.reject(testResolveCallback);
+
+    const { data, error } = await awaitWrap(promise, { rejectCallback: fn });
+
+    expect(fn).toHaveBeenCalledTimes(2);
+    expect(fn).toHaveBeenCalledWith(testResolveCallback);
   });
 
 })
